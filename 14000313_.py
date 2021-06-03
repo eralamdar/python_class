1215c1a69253e84ccf8b2351fb0eed2355f368e9
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-03 17:44:52 +0430

    Add files via upload

---
+++ 14000313_.py
@@ -0,0 +1,13 @@
+def mo_hit (tool,arz):
+    return (tool+arz*2)
+def ma_sahat(tool,arz):
+    return(tool*arz)
+
+
+tool=int(input ("please enter tool\n"))
+arz=int(input ("please enter arz\n"))
+dastoor=(input ("please enter dastoor,mohit vs masahat\n")
+if dastoor== mohit
+ mo_hit(tool,arz)
+elif
+ ma_saht(tool,arz)
