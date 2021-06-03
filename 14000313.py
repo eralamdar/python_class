d641bdd9d0e2ad72ac4a6f56f9f26f46c9aa95a4
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-03 17:22:15 +0430

    Add files via upload

---
+++ 14000313.py
@@ -0,0 +1,8 @@
+def plus(number):
+
+    return (number+1)
+print ("please enter a number")
+number=int(input())
+#umber=(input("please enter a number",number))
+number=plus(int(number))
+print(f"you are enter{number}")
