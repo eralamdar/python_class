817d49a908ef338d270dbb935e45f0620d44d35d
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-10 16:03:23 +0430

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,13 @@
+aa=0
+bb=1
+
+while bb :
+    dastoor=input("bala,paeen,ya baseh ?\n")
+    if dastoor=="bala":
+        aa+=1
+        print(f"{aa}\n")
+    if dastoor=="paeen":
+        aa-=1
+        print(f"{aa}\n")
+    if dastoor=="baseh":
+        break
