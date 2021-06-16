85aa3611ad4be9208f51d78fccc06e6991cb2455
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-16 19:25:45 +0430

    Add files via upload

---
+++ hope.py
@@ -0,0 +1,11 @@
+
+ft=(input("please Enter a number\n"))
+tt="true"
+while tt:
+    nt=(input("please Enter another number\n")
+    zz=(float(ft)+1)
+    if zz % 5 == 0:
+        if nt =="hope":
+            print("Hope bayad migofti,bakhti")
+            break
+    ft=float(nt)
