08835262e100ca9be480f1577e1a3a0c82b7b582
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-17 15:04:44 +0430

    Add files via upload

---
+++ sa2.py
@@ -0,0 +1,5 @@
+e=['amin','farokh','mahnaz','mehrana','meysam','mohamad','morteza','paniz']
+jomleh='salam man nima hastam va inja kelas pythoneh'.split ('a')
+for name in jomleh:
+    if len(name)>=1:
+        print (f"{name}")
