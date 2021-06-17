9ed8a9a6a27d8c549858ef68ce01208e4b97f6cc
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-17 16:09:58 +0430

    Add files via upload

---
+++ div4.py
@@ -0,0 +1,5 @@
+e=['amin','farokh','mahnaz','mehrana','meysam','mohamad','morteza','paniz']
+counter=0
+for index,name in enumerate(e):
+    if index%3 !=0:
+        print(f"nafar {index}{name}ast")
