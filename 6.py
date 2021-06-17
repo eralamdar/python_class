e197d050142299c17852f57b4e4f236906e029b7
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-06-17 16:25:45 +0430

    Add files via upload

---
+++ 6.py
@@ -0,0 +1,11 @@
+# e=['amin','farokh','mahnaz','mehrana','meysam','mohamad','morteza','paniz']
+# counter=0
+# for index,name in enumerate(e):
+#     if index%3 !=0:
+#         print(f"nafar {index}{name}ast")
+ii=0
+while ii <1 :
+    print(ii)
+    ii+=0.01
+# for ii in range (0,1,0.01):
+#     print(ii)
