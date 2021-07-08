ecc6f2db2aa41a02d4e419359bc4afee3e431e45
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-07-08 16:42:48 +0430

    Add files via upload

---
+++ str2.py
@@ -0,0 +1,8 @@
+class Lady:
+    def __init__(self,esm,age,daramad):
+        self.esm=esm
+        self.__age=age
+        self.daramad=daramad
+
+m=Lady("Mitra","18",2000000)
+print(m.esm , m.age,m.daramad)
