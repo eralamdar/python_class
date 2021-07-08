5ff1e81a5da308917a1239f3493d1626ce43085a
eralamdar <48651997+eralamdar@users.noreply.github.com>
2021-07-08 16:24:49 +0430

    Add files via upload

--- str.py
+++ str.py
@@ -5,6 +5,6 @@ class Number:
         return str(self.value)
     def __add__(self,other):
         return self.value + other.value+1
-    n1=Number(2)
-    n2=Number(2)
-    print(n1+n2)
+n1=Number(2)
+n2=Number(2)
+print(n1+n2)
